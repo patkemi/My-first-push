201c51cf008ed6cf9d7a52a64b7337bfab7a6bd3
unknown <brightshalom90@gmail.com>
2019-11-14 15:11:00 +0100

    First initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+// First way to write a comment
+// or */ second way to write a comment
+
+    /** third way to write a comment
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+	// write your code here
+        JavaATMMachine javaATMMachine = new JavaATMMachine();
+        javaATMMachine.main();
+
+    }
+
+}
